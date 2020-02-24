f2894c8d4862fea33a59c08820134956bcd7f3ea
FrankC <frankc@t310.org>
2020-02-24 17:11:00 +0100

    Update to swift 5, fix warnings

--- RWMRecurrenceRule/RWMRuleScheduler.swift
+++ RWMRecurrenceRule/RWMRuleScheduler.swift
@@ -464,7 +464,7 @@ public class RWMRuleScheduler {
                             let lastDayOfYear = calendar.date(from: DateComponents(year: year, month: 12, day: 31))!
                             let lastWeek = calendar.component(.weekOfYear, from: lastDayOfYear)
                             let weeksRange = calendar.range(of: .weekOfYear, in: .yearForWeekOfYear, for: startOfYear)!
-                            let weeksInYear = lastWeek == 1 ? CountableRange(weeksRange).last! : lastWeek
+                            let weeksInYear = lastWeek == 1 ? weeksRange.last! : lastWeek
                             var weekComps = DateComponents(/*year: year, */hour: yearComps.hour!, minute: yearComps.minute!, second: yearComps.second!, yearForWeekOfYear: year)
                             weekComps.weekday = rule.firstDayOfTheWeek?.rawValue ?? calendar.firstWeekday
                             for weekNo in weekNos {
@@ -538,7 +538,7 @@ public class RWMRuleScheduler {
                                             days = [startDay]
                                         } else {
                                             let range = calendar.range(of: .day, in: .month, for: startOfMonth)!
-                                            days = Array(CountableRange(range).indices)
+                                            days = Array(range.indices)
                                         }
                                         for day in days {
                                             monthComps.day = day
