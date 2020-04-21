da6108d3d5496aed4ff668ade735babb081cf98b
FrankC <frankc@t310.org>
2020-04-21 14:11:52 +0200

    Make more stuff public in the rwmrecurrencerule

--- RWMRecurrenceRule.playground/Contents.swift
+++ RWMRecurrenceRule.playground/Contents.swift
@@ -4,3 +4,13 @@ import Foundation
 import RWMRecurrenceRule
 
 // TODO - coming soon
+var dateComps = DateComponents(calendar: Calendar(identifier: Calendar.Identifier.gregorian),year:2020, month: 4, day: 18,hour: 6)
+
+dateComps.timeZone = TimeZone.current
+let startDate = Calendar.current.date(from: dateComps)!
+let rule = RWMRuleParser().parse(rule: "RRULE:FREQ=WEEKLY")!
+
+RWMRuleScheduler().enumerateDates(with: rule, startingFrom: startDate) { (date, stop) in
+    print("Date: \(date)")
+}
+print("Done")
